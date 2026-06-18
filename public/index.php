2a71c74f63ecb47cc7b1267276412d3e17f0ddc3
Htoo Myat Eain <hobocustomsoftware@gmail.com>
2026-06-18 21:41:08 +0630

    fix laravel 12 front controller setup

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -11,10 +10,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
+/** @var \Illuminate\Foundation\Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle($request = Request::capture())->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
